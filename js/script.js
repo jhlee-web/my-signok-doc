ff2049a796e87ec5fc0259ae6fb6b83a5915c541
kksuin <wlgus828@naver.com>
2022-11-14 10:26:10 +0900

    [FIX] nav data-nav 추가 Toggle theme 함수 추가 load -> html set theme 추가 current Nav is-active 추가 css 글자색 수정 튜토리얼 종료시 상하좌우 블록 삭제 추가 튜토리얼 박스 css 추가

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function checkTheme(){
-    const loadedTheme = $('html').attr('data-theme')
-    
-    loadedTheme == undefined ? $('html').attr('data-theme','light') : $('html').attr('data-theme','dark')
-}
-
-checkTheme()
-
